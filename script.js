70be0a95338c6eba67d308550335fe9e36cd2a56
zBinFinn <ermert.finn@icloud.com>
2024-02-02 17:17:10 +0100

    Funny Testing Changes

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ window.addEventListener('scroll', () => {
     start.style.right = scrollDist * height * 0.003 + 'px'
     start.style.top = scrollDist * 1 + 'px'
 });
+
+let bw_category_image = document.getElementById("photo-1")
+bw_category_image.addEventListener("click", () => {
+    location.replace("./example_photos_blackwhite.html");
+})
+
+let c_category_image = document.getElementById("photo-2")
+c_category_image.addEventListener("click", () => {
+    location.replace("./example_photos_color.html");
+})
